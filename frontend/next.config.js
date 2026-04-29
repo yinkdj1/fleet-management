a971928a0b7576a44da41382c600ba2d810e970d
dekaz llc <dekazllc@gmail.com>
2026-04-29 00:48:33 -0400

    Redirect carsgidi.com to www.carsgidi.com

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'carsgidi.com' }],
+        destination: 'https://www.carsgidi.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
   async rewrites() {
     const backendUrl = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === "production" ? "https://api.carsgidi.com" : "http://localhost:5000");
     return [
