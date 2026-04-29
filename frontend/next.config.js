3ca47609acc3e66e472dc03228eab1f5d2692ebd
dekaz llc <dekazllc@gmail.com>
2026-04-28 23:15:43 -0400

    Redirect / to /reserve

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/reserve',
+        permanent: false,
+      },
+    ];
+  },
   async rewrites() {
     const backendUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
     return [
