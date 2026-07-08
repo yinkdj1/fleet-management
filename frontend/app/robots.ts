a45f9e45e61733f932a3e2f31e4e44062a98d40f
yinkdj1 <yinkdj1@gmail.com>
2026-07-08 11:31:00 -0400

    Add robots and sitemap metadata routes

---
+++ frontend/app/robots.ts
@@ -0,0 +1,34 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://www.carsgidi.com/sitemap.xml",
+    host: "https://www.carsgidi.com",
+  };
+}
